3969aa805e1e1dda02fea121bae6fa4573749f68
mekki209 <60732190+mekki209@users.noreply.github.com>
2022-09-16 22:26:49 +0100

    Update database.js

--- config/database.js
+++ config/database.js
@@ -2,8 +2,11 @@ const mongoose = require("mongoose");
 
 
 //connect data
+const connectDatabase = () => {
 mongoose.connect('mongodb+srv://' + process.env.DB_USER_PASS+ '@livmo.raykivf.mongodb.net/BAGNGO', {
     useNewUrlParser: true,
     useUnifiedTopology: true,
 }).then(() => console.log('Connected to MongoDB'))
     .catch((err) => console.log('Failed to connect to MongooDB', err));
+}
+    module.exports = connectDatabase;
